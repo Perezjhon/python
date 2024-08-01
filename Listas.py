d50cbef0613674b48c6d2dedacfeae304e3ec535
Jhon Alexander <perezjhon908@gmail.com>
2024-07-31 22:13:00 -0500

    listas

---
+++ Listas.py
@@ -0,0 +1,46 @@
+#Añadir al final de la lista
+
+"""shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping.append('Atún')
+
+print(shopping)"""
+#Creando desde vacío
+
+"""even_numbers = []
+
+for i in range(20):
+    if i % 2 == 0:
+        even_numbers.append(i)
+
+        print(even_numbers)"""
+#ñadir en cualquier posición de una lista
+
+"""shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping.insert(1, 'Jamón')
+
+shopping
+['Agua', 'Jamón', 'Huevos', 'Aceite']
+
+shopping.insert(3, 'Queso')
+
+print(shopping)"""
+
+#Modificar una lista
+
+"""shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping[0]
+'Agua'
+
+shopping[0] = 'Jugo'
+print(shopping)"""
+
+#Borrar elementos
+
+"""shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+
+del shopping[3]
+
+print(shopping)"""
